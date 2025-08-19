2d684bb3cd45c1104520ca8b34f669bbb3c1eb48
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-19 09:46:44 -0300

    Declarando variaveis

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,33 @@
 function App() {
 
+  // Declaração de variaveis 
+  let nome = "Fiap";
+  let apelido = "Fiapinho";
+
+  let novoNome = nome.toLowerCase();
+  novoNome = nome.toUpperCase();
+
+  function soma(valor1, valor2){
+    return valor1 + valor2;
+  }
+
+  // Arrow function 
+  const desconto =(valor3, valor4)=>{
+    return valor3 - valor4
+  }
+
+  function enviar(){
+    alert(`Seja bem-vindo ${nome}`)
+  }
+
+
   return (
     <>
-    <h1>Teste</h1>
+    <h1> Seja bem-vindo! {nome}, {apelido}, {novoNome} </h1>
+    <p>A soma da função é: {soma(10,8)}</p>
+    <p>O desconto da função arrow é: {desconto(90,30)}</p>
+    <button onClick={enviar} >Clique Aqui</button>
+
     </>
   )
 }
