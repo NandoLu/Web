10c9e15fb3e64169e354b7503a1f3df076d3bafd
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-19 08:58:02 -0300

    Iniciando estrutura

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const Port = 5001;
+// 3000 - 5000
+// Porta 8080 = Internet 
+
+app.get("/teste",(req,res)=>{
+    res.semd("Olá servidor Teste ok")
+});
+
+app.get("/sistema", (req,res)=>{
+    res.send("Seja Bem-vindo ao sistema")
+});
+
+app.get("/sobre", (req,res)=>{
+    res.send("Página sobre")
+});
+
+
+app.listen(Port, ()=>{
+    console.log(`Servidor rodadno na porta http://localhost:${Port}`)
+})
